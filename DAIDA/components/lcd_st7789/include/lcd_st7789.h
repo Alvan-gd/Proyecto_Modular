a66810b55c6bb37a3399e1cd6a2dfcb5ac207d4a
Edmundo Corona <edmundocorona24@gmail.com>
2026-09-19 13:13:34 -0600

    Added LVGL to the project and a Hello World code

--- DAIDA/components/lcd_st7789/include/lcd_st7789.h
+++ DAIDA/components/lcd_st7789/include/lcd_st7789.h
@@ -8,6 +8,23 @@
 #include "driver/gpio.h"
 #include "driver/spi_master.h"
 
+// Definición nativa de pines basada en el esquema del XIAO ESP32S3
+#define LCD_HOST               SPI2_HOST
+#define PIN_NUM_DIN            9   // Pin D10 (SPI MOSI)
+#define PIN_NUM_CS             8   // Pin D9
+#define PIN_NUM_CLK            7   // Pin D8 (SPI Clock)
+#define PIN_NUM_CD             44   // Pin D7 (Data/Command)
+
+#define PIN_NUM_MISO           -1  // No requerido para solo escritura
+#define PIN_NUM_RST            -1  // Controlado con conexión a 3.3V (no se requiere pin de control)
+#define PIN_NUM_BL             -1  // Controlado con conexión a 3.3V (no se requiere pin de control)
+
+// Dimensiones 
+#define LCD_H_RES              240
+#define LCD_V_RES              280
+
+// Prototipado de funciones
+esp_lcd_panel_handle_t lcd_st7789_init(esp_lcd_panel_io_handle_t *ret_io_handle);
 
 
 #endif
--- DAIDA/components/lcd_st7789/lcd_st7789.c
+++ DAIDA/components/lcd_st7789/lcd_st7789.c
@@ -1,2 +1,45 @@
 #include "lcd_st7789.h"
 
+
+esp_lcd_panel_handle_t lcd_st7789_init(esp_lcd_panel_io_handle_t *ret_io_handle) {
+
+    spi_bus_config_t buscfg = {
+        .sclk_io_num = PIN_NUM_CLK,
+        .mosi_io_num = PIN_NUM_DIN,
+        .miso_io_num = PIN_NUM_MISO,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = LCD_H_RES * 40 * sizeof(uint16_t),
+    };
+    ESP_ERROR_CHECK(spi_bus_initialize(LCD_HOST, &buscfg, SPI_DMA_CH_AUTO));
+
+    esp_lcd_panel_io_handle_t io_handle = NULL;
+    esp_lcd_panel_io_spi_config_t io_config = {
+        .dc_gpio_num = PIN_NUM_CD,
+        .cs_gpio_num = PIN_NUM_CS,
+        .pclk_hz = 24 * 1000 * 1000, 
+        .lcd_cmd_bits = 8,
+        .lcd_param_bits = 8,
+        .spi_mode = 0,
+        .trans_queue_depth = 10,
+    };
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)LCD_HOST, &io_config, &io_handle));
+
+    esp_lcd_panel_handle_t panel_handle = NULL;
+    esp_lcd_panel_dev_config_t panel_config = {
+        .reset_gpio_num = PIN_NUM_RST,
+        .bits_per_pixel = 16,
+    };
+    ESP_ERROR_CHECK(esp_lcd_new_panel_st7789(io_handle, &panel_config, &panel_handle));
+
+    ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_set_gap(panel_handle, 0, 20)); 
+    ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
+
+    if (ret_io_handle) {
+        *ret_io_handle = io_handle;
+    }
+
+    return panel_handle;
+}
